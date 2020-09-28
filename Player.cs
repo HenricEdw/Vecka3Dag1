c196ad7485571df823ff3cc740a1fa611a8d7b9b
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-09-28 13:01:38 +0200

    Added first commit
    FIrst commit

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerExample
+{
+    class Player
+    {
+        private string m_playerName;
+        private int m_score;
+        private int m_timePlayed;
+
+        public int TimePlayed
+        {
+            get { return m_timePlayed; }
+            set { m_timePlayed = value; }
+        }
+
+        public string PlayerName
+        {
+            get { return m_playerName; }
+            set { m_playerName = value; }
+        }
+
+        public int Score
+        {
+            get { return m_score; }
+            set { m_score = value; }
+        }
+
+        public int ScoreByTimePlayed
+        {
+            get { return m_score / m_timePlayed; }
+        }
+
+        //Autoproperty
+        public int  { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa instans
+            Player player = new Player();
+
+            //Testa att ändra och läsa ut lite värden via properties
+            //Debugga gärna!
+            player.PlayerName = "Nytt Namn";
+            player.Score = 1000;
+            player.TimePlayed = 10;
+            var scoreTime = player.ScoreByTimePlayed;
+
+            Console.WriteLine($"Spelare {player.PlayerName} har {player.Score} poäng!");
+            Console.ReadLine();
+        }
+    }
+}
